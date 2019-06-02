70ccc88666ed0fa17a0e3ec7b4b8fabf24b57322
KateyMG <46852046+KateyMG@users.noreply.github.com>
2019-06-02 11:30:48 -0600

    KatherineMazariegos_Lab01
    Laboratorio_1_31_05_2019

---
+++ Lab01/Lab01.py
@@ -0,0 +1,74 @@
+#Ejercicio_1
+print("****EJERCICIO 1****\n")
+Num1=100    #Crear tres variables y asignar valores
+Num2= 10
+Resultado= 0
+print("Num1 :" +str(Num1))
+print("Num2 :" +str(Num2))
+print("El resultado es: " +str(Resultado))
+
+print()
+
+#Ejercicio_2_y_3
+print("****EJERCICIO 2 y 3****\n")
+Suma= Num1+Num2    #Sumar dos variables
+Resultado= Suma
+print("El resultado de la suma entre Num1 y Num2 es: "+str(Resultado)+"\n")
+Resta= Num1-Num2   #Restar las dos variables
+Resultado = Resta
+print("El resultado de la resta entre Num1 y Num2 es: "+str(Resultado)+"\n")
+Mult= Num1*Num2    #Multiplicar dos variables
+Resultado=Mult
+print("El resultado de la multiplicación entre Num1 y Num2 es: "+ str(Resultado)+"\n")
+Div= Num1/Num2     #Dividir dos variables
+Resultado=Div
+print("El resultado de la división entre Num1 y Num2 es: "+ str(Resultado)+"\n")
+Pot=Num1**Num2     #Elevar var1 a var2
+Resultado=Pot
+print("El resultado de la potencia entre Num1 y Num2 es: "+ str(Resultado)+"\n")
+
+#Ejercicio_4_Matriz de 5*5
+print("****EJERCICIO 4****\n")    #Crear una matriz de 5*5
+M= [[1,2,3,4,5], [5,4,3,2,1],[6,7,8,9,0],[1,2,3,4,5],[0,9,8,7,6]]
+print ("M= " +str(M)+"\n")
+
+#Ejercicio_5
+print("****EJERCICIO 5****\n")   #Imprimir matriz
+print("M[0] = " + str(M[0]))
+print("M[1] = " + str(M[1]))
+print("M[2] = " + str(M[2]))
+print("M[3] = " + str(M[3]))
+print("M[4] = " + str(M[4]))
+
+
+#Ejercicio_6
+print("")
+print("****EJERCICIO 6****\n")
+M [0][0] = 0      #Cambiar valores a la diagonal principal
+M [1][1] = 0
+M [2][2] = 0
+M [3][3] = 0
+M [4][4] = 0
+
+print ("M[0][0]=" +str(M[0][0]))
+print ("M[1][1]=" +str(M[1][1]))
+print ("M[2][2]=" +str(M[2][2]))
+print ("M[3][3]=" +str(M[3][3]))
+print ("M[4][4]=" +str(M[4][4]))
+
+#Ejercicio_7
+print("")
+print("****EJERCICIO 7****\n")   #Imprimir la nueva matriz
+print("M[0] = " + str(M[0]))
+print("M[1] = " + str(M[1]))
+print("M[2] = " + str(M[2]))
+print("M[3] = " + str(M[3]))
+print("M[4] = " + str(M[4]))
+
+
+
+
+
+
+
+
